9bedaa350479e787bed5327c7ce466394a7a9930
MattFerreira18 <matheusferreira.dev@gmail.com>
2022-09-14 00:16:22 -0300

    refactor: create factory to make SUT and spyes

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,19 +1,30 @@
 import { HttpPostClientSpy } from "../../test/mock-http-client";
 import RemoteAuthentication from "./remote-authentication";
 
+type SutTypes = {
+  sut: RemoteAuthentication;
+  httpPostClientSpy: HttpPostClientSpy;
+};
+
+const makeSut = (url = "https://www.api.com.br"): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy();
+  /**
+   * S -> system
+   * U -> under
+   * T -> test
+   */
+  const sut = new RemoteAuthentication(url, httpPostClientSpy);
+
+  return { sut, httpPostClientSpy };
+};
+
 describe("remote authentication", () => {
   it("should be able to call HttpPostClient with correct URL", async () => {
     const url = "https://www.api.com.br";
-    const httpClientSpy = new HttpPostClientSpy();
-    /**
-     * S -> system
-     * U -> under
-     * T -> test
-     */
-    const sut = new RemoteAuthentication(url, httpClientSpy);
+    const { sut, httpPostClientSpy } = makeSut(url);
 
     await sut.auth();
 
-    expect(httpClientSpy.url).toBe(url);
+    expect(httpPostClientSpy.url).toBe(url);
   });
 });
